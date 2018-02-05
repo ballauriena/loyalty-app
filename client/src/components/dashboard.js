f0ea507570088a99cfc8aadd001ad232718b778c
Laura Streb <lstreb@fanatics.com>
2018-02-04 17:19:45 -0700

    Dashboard UI cleanup

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -7,28 +7,38 @@ function handleCheckout() {
 const Dashboard = {
 	view: function() {
 		return m("div", { class: "ui raised very padded text container segment" }, [
-			m("h1", { class: "ui icon header" }, [
-				m("i", { class: "star half empty icon" }),
-				m("div", { class: "content" }, [
-					`${state.store.user.first_name}'s Dashboard`,
-					m("div", { class: "sub header" }, "View your points and checkin total here")
+			m("div", { class: "ui four column doubling stackable grid container" }, [
+				m("div", { class: "ui sixteen wide column center aligned segment"}, [
+					m("h1", { class: "ui icon header" }, [
+						m("i", { class: "star half empty icon" }),
+						m("div", { class: "content" }, [
+							`${state.store.user.first_name}'s Dashboard`,
+							m("div", { class: "sub header" }, "View your points and checkin total here")
+						])
+					])
+				]),
+				m("div", { class: "ui eight wide column center aligned segment" }, [
+					m("h3", { class: "ui icon header" }, [
+						m("i", { class: "thumbs outline up icon" }),
+						m("div", { class: "content" }, [
+							state.store.totalPoints,
+							m("div", { class: "sub header" }, "POINTS")
+						])
+					])
+				]),
+				m("div", { class: "ui eight wide column center aligned segment" }, [
+					m("h3", { class: "ui icon header" }, [
+						m("i", { class: "hand spock icon" }),
+						m("div", { class: "content" }, [
+							state.store.totalCheckins,
+							m("div", { class: "sub header" }, "CHECKINS")
+						])
+					])
+				]),
+				m("div", { class: "ui sixteen wide column"}, [
+					m("button", { class: "ui basic massive olive right floated button", onclick: handleCheckout }, "Checkout")
 				])
-			]),
-			m("h3", { class: "ui icon header" }, [
-				m("i", { class: "thumbs outline up icon" }),
-				m("div", { class: "content" }, [
-					state.store.totalPoints,
-					m("div", { class: "sub header" }, "POINTS")
-				])
-			]),
-			m("h3", { class: "ui icon header" }, [
-				m("i", { class: "hand spock icon" }),
-				m("div", { class: "content" }, [
-					state.store.totalCheckins,
-					m("div", { class: "sub header" }, "CHECKINS")
-				])
-			]),
-			m("button", { class: "ui basic massive olive button", onclick: handleCheckout }, "Checkout")
+			])
 		]);
 	}
 }
